78ee7008907202863d7f5945a69e233e7d5def3c
queens kisivuli <queenskisivuli@gmail.com>
2023-04-24 00:06:41 +0300

    solution to diagonal

---
+++ Day-2/Diagonal-Difference/solution.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    # Write your code here
+    arr_len = len(arr)
+    left_diag = sum([arr[i][i] for i in range(arr_len)])
+    right_diag = sum([arr[i][arr_len-i-1] for i in range(arr_len)])
+    return abs(left_diag - right_diag)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
